4ee1ed20edacf46327ae56d5a56556e0601358e2
jvarone <jvarone@webfx.com>
2025-03-14 22:59:21 +0000

    Randomness, banner

--- scripts.js
+++ scripts.js
@@ -27,41 +27,46 @@ document.addEventListener("DOMContentLoaded", function () {
                 rowCount % 2 === 0 ? "Image_Left-1.png" : "Image_Left-2.png",
             ];
             let lastRandomNumber2 = 1;
+            let imagesInRow = [];
             for (let i = 0; i < NUM_IMAGES_PER_ROW - 2; i++) {
                 if (randomNumber > 20) {
                     let randomNumber2 = Math.floor(Math.random() * 8) + 1;
-                    while (randomNumber2 === lastRandomNumber2) {
+                    while (randomNumber2 === lastRandomNumber2 || imagesInRow.includes(randomNumber2)) {
                         randomNumber2 = Math.floor(Math.random() * 8) + 1;
                     }
                     lastRandomNumber2 = randomNumber2;
                     images.push(`Image_R-${randomNumber2}.png`);
+                    imagesInRow.push(randomNumber2);
                 } else if (randomNumber > 10) {
                     // Generate a random number between 9 and 12
                     let randomNumber2 = Math.floor(Math.random() * 4) + 9;
-                    while (randomNumber2 === lastRandomNumber2) {
+                    while (randomNumber2 === lastRandomNumber2 || imagesInRow.includes(randomNumber2)) {
                         randomNumber2 = Math.floor(Math.random() * 4) + 9;
                     }
                     lastRandomNumber2 = randomNumber2;
                     images.push(`Image_R-${randomNumber2}.png`);
-                } else if (randomNumber > 5) {
+                    imagesInRow.push(randomNumber2);
+                } else if (randomNumber > 1) {
                     // Generate a random number between 13 and 15
                     let randomNumber2 = Math.floor(Math.random() * 3) + 13;
-                    while (randomNumber2 === lastRandomNumber2) {
+                    while (randomNumber2 === lastRandomNumber2 || imagesInRow.includes(randomNumber2)) {
                         randomNumber2 = Math.floor(Math.random() * 3) + 13;
                     }
                     lastRandomNumber2 = randomNumber2;
                     images.push(`Image_R-${randomNumber2}.png`);
-                } else if (lastRandomNumber2 !== 16) {
+                    imagesInRow.push(randomNumber2);
+                } else if (lastRandomNumber2 !== 16 && !imagesInRow.includes(16)) {
                     images.push(`Image_R-16.png`);
                     lastRandomNumber2 = 16;
                 } else {
                     // Just do any random number between 1 and 16
                     let randomNumber2 = Math.floor(Math.random() * 16) + 1;
-                    while (randomNumber2 === lastRandomNumber2) {
+                    while (randomNumber2 === lastRandomNumber2 || imagesInRow.includes(randomNumber2)) {
                         randomNumber2 = Math.floor(Math.random() * 16) + 1;
                     }
                     lastRandomNumber2 = randomNumber2;
                     images.push(`Image_R-${randomNumber2}.png`);
+                    imagesInRow.push(randomNumber2);
                 }
             }
             images.push(
