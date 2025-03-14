28d9c4c279a6d9bbb9e98625451575509142686f
jvarone <jvarone@webfx.com>
2025-03-14 22:53:23 +0000

    Prevent same number twice

--- scripts.js
+++ scripts.js
@@ -11,9 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
         if (scrollPosition - furthestScroll >= 211) {
         let numberOfRows = (scrollPosition - furthestScroll) / 211;
         furthestScroll = scrollPosition;
+        let lastRandomNumber = 1;
         for (let i = 0; i < numberOfRows; i++) {
             // Generate a random number between 1 and 100
             const randomNumber = Math.floor(Math.random() * 100) + 1;
+            while (randomNumber === lastRandomNumber) {
+                randomNumber = Math.floor(Math.random() * 100) + 1;
+            }
+            lastRandomNumber = randomNumber;
             console.log("scroll", randomNumber);
             const table = document.querySelector("table");
             const row = document.createElement("tr");
@@ -21,19 +26,42 @@ document.addEventListener("DOMContentLoaded", function () {
             let images = [
                 rowCount % 2 === 0 ? "Image_Left-1.png" : "Image_Left-2.png",
             ];
+            let lastRandomNumber2 = 1;
             for (let i = 0; i < NUM_IMAGES_PER_ROW - 2; i++) {
                 if (randomNumber > 20) {
-                    images.push(`Image_R-${Math.floor(Math.random() * 8) + 1}.png`);
+                    let randomNumber2 = Math.floor(Math.random() * 8) + 1;
+                    while (randomNumber2 === lastRandomNumber2) {
+                        randomNumber2 = Math.floor(Math.random() * 8) + 1;
+                    }
+                    lastRandomNumber2 = randomNumber2;
+                    images.push(`Image_R-${randomNumber2}.png`);
                 } else if (randomNumber > 10) {
                     // Generate a random number between 9 and 12
-                    const randomNumber2 = Math.floor(Math.random() * 4) + 9;
+                    let randomNumber2 = Math.floor(Math.random() * 4) + 9;
+                    while (randomNumber2 === lastRandomNumber2) {
+                        randomNumber2 = Math.floor(Math.random() * 4) + 9;
+                    }
+                    lastRandomNumber2 = randomNumber2;
                     images.push(`Image_R-${randomNumber2}.png`);
                 } else if (randomNumber > 5) {
                     // Generate a random number between 13 and 15
-                    const randomNumber3 = Math.floor(Math.random() * 3) + 13;
-                    images.push(`Image_R-${randomNumber3}.png`);
-                } else {
+                    let randomNumber2 = Math.floor(Math.random() * 3) + 13;
+                    while (randomNumber2 === lastRandomNumber2) {
+                        randomNumber2 = Math.floor(Math.random() * 3) + 13;
+                    }
+                    lastRandomNumber2 = randomNumber2;
+                    images.push(`Image_R-${randomNumber2}.png`);
+                } else if (lastRandomNumber2 !== 16) {
                     images.push(`Image_R-16.png`);
+                    lastRandomNumber2 = 16;
+                } else {
+                    // Just do any random number between 1 and 16
+                    let randomNumber2 = Math.floor(Math.random() * 16) + 1;
+                    while (randomNumber2 === lastRandomNumber2) {
+                        randomNumber2 = Math.floor(Math.random() * 16) + 1;
+                    }
+                    lastRandomNumber2 = randomNumber2;
+                    images.push(`Image_R-${randomNumber2}.png`);
                 }
             }
             images.push(
